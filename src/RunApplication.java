65da7ca1a2145fb6d46c17f25ec90d6e05d3ffee
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-04 09:49:11 -0500

    Feat: start game logic

--- src/RunApplication.java
+++ src/RunApplication.java
@@ -1,20 +1,38 @@
+import characters.Player;
+import characters.Villain;
+
+import java.util.Random;
+
 public class RunApplication {
 
     static UserInput in = new UserInput();
+    static Random randomNum = new Random();
+
+    //method that gives a number between 1-10 for attack probability
+    static int attackProbability(){
+        return randomNum.nextInt(10);
+    }
+
+    //switch case for left or right
+
+
+    Player hero;
+    Villain villain;
+
+    private void gameFlow(){
+        System.out.println("You wake up in an unfamiliar room. \n Confused with a pounding head you decide to sit up. \n What do you do next?");
+
+
+
+
+
+    }
+
+
+
 
     public static void main(String[] args) {
 
-//        System.out.println("Enter a string: ");
-//        String userString = in.getString();
-//        System.out.println("You entered: " + userString);
-//
-//        System.out.println("Enter a number: ");
-//        int userInt = in.getInt();
-//        System.out.println(userInt);
-
-        System.out.println("Enter yes or no");
-        boolean userBoolean = in.yesNo();
-        System.out.println(userBoolean);
     }
 
 }
