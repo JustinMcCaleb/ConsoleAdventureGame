cdcbf49a5ae4d38c4de572315e245726a173636b
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-07 12:10:06 -0500

    added constructor

--- src/characters/Alien.java
+++ src/characters/Alien.java
@@ -6,5 +6,9 @@ public class Alien {
     private String type;
     private boolean isFriendly;
 
-
+    //constructor
+    public Alien(String type, boolean isFriendly){
+        this.type = type;
+        this.isFriendly = isFriendly;
+    }
 }
