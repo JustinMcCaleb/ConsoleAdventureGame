4da5118f605752c8de16db19d5eb443d5cf805bf
Justin McCaleb <justinleemccaleb@gmail.com>
2020-09-07 12:15:18 -0500

    added demon class

---
+++ src/characters/Demon.java
@@ -0,0 +1,10 @@
+package characters;
+
+public class Demon extends Villain{
+
+
+    public Demon(String name, int health, int attackPoints) {
+        super(name, health, attackPoints);
+    }
+
+}
